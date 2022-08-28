3a95df58421f783be8ebb6910498b58919d2e095
nmatynia <norbert.matyniaa@gmail.com>
2022-08-28 13:18:00 +0200

    wip api call + styling

--- src/App.tsx
+++ src/App.tsx
@@ -17,8 +17,8 @@ import { AutoComplete } from './components/AutoComplete';
 
 function App() {
   return (
-    <div className="flex flex-col items-center mt-16">
-      <h1 className="text-4xl font-bold">Norbert Matynia's AutoComplete</h1>
+    <div className='flex flex-col items-center mt-16'>
+      <h1 className='text-4xl font-bold'>Norbert Matynia's AutoComplete</h1>
       <AutoComplete/>
     </div>
   );
--- src/components/AutoComplete.tsx
+++ src/components/AutoComplete.tsx
@@ -1,18 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 // https://docs.github.com/en/rest/search#search-users
+// https://api.github.com/search/users
+
 export const AutoComplete = () => {
+  const [search, setSearch] = useState<string>('')
+  const [fetchedData, setFetchedData] = useState<Array<any>>([])
+
+  const handleChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => setSearch(e.target.value)
+
+  const fetch = (_search: string) => {
+    setFetchedData([])
+
+    if(_search.length < 2) return;
+
+    Promise.all(
+      [
+        axios.get(`https://api.github.com/search/users?q=${_search}&per_page=50`).then(res => res.data.items),
+        // axios.get(`https://api.github.com/search/repositories?q=${_search}&per_page=50`).then(res => res.data.items)
+      ]
+    ).then(responses => {
+      for (const response of responses) {
+        setFetchedData(fetchedData.concat(response))
+      }
+    })
+  }
+
+  useEffect(() => {
+    fetch(search)
+  }, [search])
+
   return (
-    <div>
-        <input type="text" className='w-[500px] h-9 border-2 pl-2 mt-16 outline-none rounded border-gray-400 focus-visible:border-gray-800'/>
+    <div className="relative">
+      <input
+        type='text'
+        className='w-[500px] h-9 border-2 pl-2 mt-16 outline-none rounded border-gray-400 focus-visible:border-gray-800'
+        value={search}
+        onChange={handleChangeSearch}
+      />
 
-        <div>
-            <div>
+      <div className='absolute w-full h-52 overflow-hidden overflow-y-scroll shadow-2xl rounded'>
+        {fetchedData.map((results,idx)=>(
+          <div key={idx} className='m-3 border-b-2 last:border-b-0 border-border-gray-400'>
+            {results.login}
+          </div>
+        ))}
+      </div>
 
-            </div>
-        </div>
-        
     </div>
   )
 }
