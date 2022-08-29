ae9c6e4dd65ea3eb504e8e8e486fc891c029db0a
nmatynia <norbert.matyniaa@gmail.com>
2022-08-29 10:35:32 +0200

    working request / better styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,6 @@ import React from 'react';
 
 import { AutoComplete } from './components/AutoComplete';
 
-//TODO
-// - Minimal chars number to initialize search: 3.
-
-// - Result items are combined and displayed alphabetically using repository and profile name as ordering keys.
-
-// - Number of result items should be limited to 50 per request.
-
-// - The component should give visual feedback for when the data is being fetched, the results are empty, or the request resulted in an error.
-
-// - The component supports keyboard strokes (up and down arrows to browse the results, enter to open a new tab with the repository/user page).
-
-// - The solution should also display a meaningful snippet of your ability to test the code.
-
 function App() {
   return (
     <div className='flex flex-col items-center mt-16'>
--- src/components/AutoComplete.tsx
+++ src/components/AutoComplete.tsx
@@ -4,49 +4,82 @@ import axios from 'axios'
 // https://docs.github.com/en/rest/search#search-users
 // https://api.github.com/search/users
 
+//TODO
+// - Minimal chars number to initialize search: 3. - DONE
+
+// - Result items are combined and displayed alphabetically using repository and profile name as ordering keys. - DONE
+
+// - Number of result items should be limited to 50 per request. - DONE
+
+// - The component should give visual feedback for when the data is being fetched, the results are empty, or the request resulted in an error.
+
+// - The component supports keyboard strokes (up and down arrows to browse the results, enter to open a new tab with the repository/user page).
+
+// - The solution should also display a meaningful snippet of your ability to test the code.
+
+//NOTES
+// 403 is bugging the app try doing something with it
+// Make repos alphabetically higher (delete ../.. from repos name (?))
+// Make using arrow a thing
+// Loading icon 
+// Add user photo? Repos / Info ?
+// Change 'a' tag placement and its style on hover, focus
+// Clicking outside is closing the dropdown
+// Make hrefs open another tab with the repo/user - DONE
+
 export const AutoComplete = () => {
+
   const [search, setSearch] = useState<string>('')
   const [fetchedData, setFetchedData] = useState<Array<any>>([])
+  const [loading, setLoading] = useState<boolean>(false);
 
   const handleChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => setSearch(e.target.value)
 
-  const fetch = (_search: string) => {
-    setFetchedData([])
 
-    if(_search.length < 2) return;
+  //Fetching data
+  const fetch = async (_search: string, pageLimit: number = 50) => {
 
-    Promise.all(
-      [
-        axios.get(`https://api.github.com/search/users?q=${_search}&per_page=50`).then(res => res.data.items),
-        // axios.get(`https://api.github.com/search/repositories?q=${_search}&per_page=50`).then(res => res.data.items)
-      ]
-    ).then(responses => {
-      for (const response of responses) {
-        setFetchedData(fetchedData.concat(response))
-      }
-    })
+    if (_search.length < 3) {
+      setFetchedData([]); 
+      return;
+    };
+
+    const [users, repos] = await Promise.allSettled([
+      axios.get(`https://api.github.com/search/users?q=${_search}&per_page=${pageLimit}`),
+      axios.get(`https://api.github.com/search/repositories?q=${_search}&per_page=${pageLimit}`)
+    ]) as { status: 'fulfilled' | 'rejected', value: any }[];
+
+    setFetchedData([...users.value.data.items, ...repos.value.data.items].sort())
   }
+  //
 
   useEffect(() => {
     fetch(search)
   }, [search])
 
+
   return (
     <div className="relative">
       <input
         type='text'
+        placeholder='Search for Github users &#38; repos'
         className='w-[500px] h-9 border-2 pl-2 mt-16 outline-none rounded border-gray-400 focus-visible:border-gray-800'
         value={search}
         onChange={handleChangeSearch}
       />
 
-      <div className='absolute w-full h-52 overflow-hidden overflow-y-scroll shadow-2xl rounded'>
-        {fetchedData.map((results,idx)=>(
-          <div key={idx} className='m-3 border-b-2 last:border-b-0 border-border-gray-400'>
-            {results.login}
-          </div>
-        ))}
-      </div>
+      { fetchedData.length > 0 && 
+        <div className='absolute w-full h-52 overflow-hidden overflow-y-scroll shadow-2xl rounded'>
+          {fetchedData.map((results, idx) => (
+            <div key={idx} className='mx-3 py-3 border-b-2 last:border-b-0 border-border-gray-400 hover:text-gray-400'>
+              <a href={results.html_url}  target='_blank' rel="noopener noreferrer" className="w-full">
+                {results.login ?? results.full_name}
+              </a>
+            </div>
+
+          ))}
+        </div>
+      }
 
     </div>
   )
